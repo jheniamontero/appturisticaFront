2794dbb993b70ff61b3f18c104db7d7ee4d32a2b
jheniamontero <jheniamontero@gmail.com>
2022-09-20 22:11:26 -0500

    frontend desarrollo

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { IndexComponent } from './index/index.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PrincipalComponent,
+    IndexComponent,
+    UsuarioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
